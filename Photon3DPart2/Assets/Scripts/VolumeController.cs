af3dd09c6fc476003e7a6e1294ecfac6e4f1490e
Aidi <aidi.saifudin@gmail.com>
2021-12-24 16:35:52 +0800

    VOLUME SLIDER DONE

--- Photon3DPart2/Assets/Scripts/VolumeController.cs
+++ Photon3DPart2/Assets/Scripts/VolumeController.cs
@@ -9,11 +9,30 @@ public class VolumeController : MonoBehaviour
     public Slider volumeSlider = null;
     public TextMeshProUGUI volumeTextUI = null;
 
+    public void Start()
+    {
+        LoadValues();
+    }
+
 
     public void VolumeSlider(float volume)
     {
         volumeTextUI.text = volume.ToString("0.0");
     }
 
+
+    public void SaveVolumeButton()
+    {
+        float volumeValue = volumeSlider.value;
+        PlayerPrefs.SetFloat("VolumeValue", volumeValue);
+        LoadValues();
+    }
+
+    public void LoadValues()
+    {
+        float volumeValue = PlayerPrefs.GetFloat("VolumeValue");
+        volumeSlider.value = volumeValue;
+        AudioListener.volume = volumeValue;
+    }
    
 }
