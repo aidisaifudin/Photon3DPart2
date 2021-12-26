559d90e4990f779a63ed1354c59de11b3465dab8
Toastyship <64509653+Toastyship@users.noreply.github.com>
2021-12-26 21:22:37 +0800

    Psh
    s

--- Photon3DPart2/Assets/Scripts/OnImpact.cs
+++ Photon3DPart2/Assets/Scripts/OnImpact.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class OnImpact : MonoBehaviour
 {
-    public GameObject impactEffect;
+   // public GameObject impactEffect;
 
     public float damage = 25f;
     // Start is called before the first frame update
@@ -21,7 +21,7 @@ public class OnImpact : MonoBehaviour
     private void OnCollisionEnter(Collision collision)
     {
         ContactPoint contact = collision.contacts[0];
-        Instantiate(impactEffect, contact.point, Quaternion.LookRotation(contact.normal));
+       // Instantiate(impactEffect, contact.point, Quaternion.LookRotation(contact.normal));
 
         if (collision.gameObject.tag == "Enemy")
         {
