0dd87f85fe55bd74ba9c46657f1b1cf59aefc4a4
Toastyship <64509653+Toastyship@users.noreply.github.com>
2022-01-02 17:56:46 +0800

    Test

--- Photon3DPart2/Assets/Scripts/DoorKey.cs
+++ Photon3DPart2/Assets/Scripts/DoorKey.cs
@@ -25,14 +25,29 @@ public class DoorKey : MonoBehaviour
     {
         if (inTrigger)
         {
+            
+        }
+    }
+
+    public void OnTriggerEnter(Collision other)
+    {
+        if (gameObject.tag == "Player")
+        {
+            GUI.Box(new Rect(0, 60, 200, 25), "Press E to take key");
             if (Input.GetKeyDown(KeyCode.E))
             {
                 DoorScripts.doorKey = true;
-                Destroy(this.gameObject);
+                
             }
-            PhotonView photonView = PhotonView.Get(this);
-            photonView.RPC("Update", RpcTarget.All);
         }
+    }
+    [PunRPC]
+    public void TakeKey()
+    {
+        DoorScripts.doorKey = true;
+        Destroy(this.gameObject);
+        Destroy(this.gameObject); PhotonView photonView = PhotonView.Get(this);
+        photonView.RPC("TakeKey", RpcTarget.All);
     }
 
     void OnGUI()
