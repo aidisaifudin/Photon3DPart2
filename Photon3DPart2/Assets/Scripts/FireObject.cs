6d12724660972906ad58ffd61c809404df986c9e
Toastyship <64509653+Toastyship@users.noreply.github.com>
2022-01-04 22:40:11 +0800

    Test
    test

--- Photon3DPart2/Assets/Scripts/FireObject.cs
+++ Photon3DPart2/Assets/Scripts/FireObject.cs
@@ -1,15 +1,18 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Photon.Pun;
+using Photon.Realtime;
 
-public class FireObject : MonoBehaviour
+public class FireObject : MonoBehaviourPun
 {
     public GameObject Bullet;
     public float Force = 2000f;
+    public PhotonView pV;
     // Start is called before the first frame update
     void Start()
     {
-        
+        pV = GetComponent<PhotonView>();
     }
 
     // Update is called once per frame
