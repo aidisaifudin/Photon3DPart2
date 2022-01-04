434b6d909589e67250925ceed8b0d3dea7af8020
Toastyship <64509653+Toastyship@users.noreply.github.com>
2022-01-04 20:34:29 +0800

    Test
    test

--- Photon3DPart2/Assets/Scripts/RotatingKey.cs
+++ Photon3DPart2/Assets/Scripts/RotatingKey.cs
@@ -1,16 +1,49 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Photon.Pun;
 
 public class RotatingKey : MonoBehaviour
 {
     public int rotatingSpeed;
-
+    private bool pickUpAllowed;
     
 
     // Update is called once per frame
     void Update()
     {
         transform.Rotate(0, rotatingSpeed, 0, Space.World);
+        if(pickUpAllowed && Input.GetKeyDown(KeyCode.E))
+        {
+            PickUp();
+        }
+    }
+    public void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == ("Player"))
+        {
+            pickUpAllowed = true;
+            Debug.Log("Key Here");
+        }
+    }
+    public void OnTriggerExit(Collider other)
+    {
+        if (other.gameObject.tag == ("Player"))
+        {
+            pickUpAllowed = false;
+        }
+    }
+    
+    [PunRPC]
+    public void PickUp()
+    {
+        Destroy(gameObject);
+        Debug.Log("Key Gone");
+    }
+    private void UpdatePickUp()
+    {
+        PhotonView photonView = PhotonView.Get(this);
+        photonView.RPC("PickUp", RpcTarget.All);
+
     }
 }
